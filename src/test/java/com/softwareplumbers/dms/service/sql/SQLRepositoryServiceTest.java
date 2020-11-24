eb926da4c9318edb4f9734656cc36e3cad622d19
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2020-11-23 23:32:34 -0500

    Add test for EXTERNAL_REFERENCE createOrUpdateOption

--- src/test/java/com/softwareplumbers/dms/service/sql/SQLRepositoryServiceTest.java
+++ src/test/java/com/softwareplumbers/dms/service/sql/SQLRepositoryServiceTest.java
@@ -1,18 +1,33 @@
 package com.softwareplumbers.dms.service.sql;
 
+import com.softwareplumbers.common.pipedstream.InputStreamSupplier;
 import com.softwareplumbers.common.sql.FluentStatement;
 import com.softwareplumbers.common.sql.Schema;
+import com.softwareplumbers.dms.Document;
+import com.softwareplumbers.dms.DocumentLink;
+import com.softwareplumbers.dms.Exceptions;
+import com.softwareplumbers.dms.Options;
 import java.util.UUID;
 
 import org.junit.Before;
 
 import com.softwareplumbers.dms.common.test.BaseRepositoryServiceTest;
 import com.softwareplumbers.dms.Reference;
+import com.softwareplumbers.dms.RepositoryPath;
 import com.softwareplumbers.dms.RepositoryService;
+import com.softwareplumbers.dms.common.impl.StreamInfo;
 import com.softwareplumbers.dms.common.test.TestModel;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Optional;
+import java.util.Random;
+import javax.json.JsonValue;
 import org.junit.After;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -25,6 +40,12 @@ public class SQLRepositoryServiceTest extends BaseRepositoryServiceTest {
 	
     @Autowired
 	public SQLRepositoryService service;
+    
+    @Autowired
+    DocumentDatabase factory;
+    
+    @Autowired
+    Filestore<Id> filestore;
     
     int connectionCount = 0;
 
@@ -67,6 +88,26 @@ public class SQLRepositoryServiceTest extends BaseRepositoryServiceTest {
     @After
     public void setCountAfter() {
         assertEquals(connectionCount, FluentStatement.getConnectionCount());
+    }
+    
+    @Test
+    public void testExternalReferenceUpdatesMetadata() throws SQLException, IOException, Exceptions.InvalidObjectName, Exceptions.InvalidReference, Exceptions.InvalidWorkspace, Exceptions.InvalidWorkspaceState {
+        
+        Id version = filestore.generateKey();
+        InputStreamSupplier data = InputStreamSupplier.markPersistent(()->new ByteArrayInputStream("test123".getBytes()));
+        StreamInfo streamInfo = StreamInfo.of(data);
+        filestore.put(version, null, streamInfo);
+        Id id = new Id();
+        try (DatabaseInterface api = factory.getInterface()) {
+            byte[] wrongDigest = new byte[32];
+            new Random().nextBytes(wrongDigest);        
+            api.createDocument(id, version, "text/plain", streamInfo.length, wrongDigest, JsonValue.EMPTY_JSON_OBJECT);
+            api.commit();
+        }
+        
+        DocumentLink link = service().createDocumentLink(RepositoryPath.ROOT.add("directory","hello.txt"), new Reference(id.toString(), version.toString()), Options.EXTERNAL_REFERENCE, Options.CREATE_MISSING_PARENT);
+        
+        assertArrayEquals(streamInfo.digest, link.getDigest());
     }
 
 }
